1b937f1ff61f6cae85db18823bb307cb46fc43b7
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2024-07-20 12:38:15 +0900

    add test

--- test_q.py
+++ test_q.py
@@ -1,10 +1,19 @@
 import q
 
 
-# content of test_sample.py
-def func(x):
-    return x + 1
+def test_prompt_alan_turing():
+    prompt = "Alan Turing theorized that computers would one day become"
+    r = q.run(
+        prompt=prompt,
+        n_tokens_to_generate=40,
+        model_size="124M",
+        models_dir="models",
+    )
 
-
-def test_answer():
-    assert func(3) == 4
+    assert r.tps > 0
+    assert (
+        r.output_text
+        == " the most powerful machines on the planet.\n\n"
+        + "The computer is a machine that can perform complex calculations, "
+        + "and it can perform these calculations in a way that is very similar to the human brain.\n"
+    )
