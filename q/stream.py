6b19d82cf6bacf7be895bb1d16ba232ae8726114
Takanori Ishikawa <takanori.ishikawa@gmail.com>
2025-03-15 15:23:47 +0900

    fix

--- q/stream.py
+++ q/stream.py
@@ -1,14 +1,17 @@
+from typing import Callable, Optional
+
+from q.encoder import Encoder
+
+
 class TokenStreamHandler:
     """
     トークンストリームを処理するハンドラークラス
     BPEトークナイザーの問題に対処するためにバッファリング方式を使用
-
-    TODO: 日本語などのマルチバイト文字のデコード問題を解決する。BPEトークナイザーは単語の部分（サブ
-    ワード）を扱うため、日本語のような非ラテン文字では1つの文字が複数のトークンに分割される可能性が
-    あります。そのため、個々のトークンをデコードすると不完全な文字になることがあります。
     """
 
-    def __init__(self, encoder, callback=None):
+    def __init__(
+        self, encoder: Encoder, callback: Optional[Callable[[str], None]] = None
+    ):
         """
         Args:
             encoder: トークンをデコードするエンコーダー
@@ -16,7 +19,7 @@ class TokenStreamHandler:
         """
         self.encoder = encoder
         self.callback = callback
-        self.token_buffer = []
+        self.token_buffer: list[int] = []
         self.last_text = ""
 
     def __call__(self, tokens: list[int]) -> bool:
@@ -34,16 +37,23 @@ class TokenStreamHandler:
             self.token_buffer.extend(tokens)
 
             # 全トークンをデコード
-            current_text = self.encoder.decode(self.token_buffer)
+            #
+            # NOTE: BPEトークナイザーは単語の部分（サブワード）を扱うため、日本語のような非ラテン文
+            # 字では1つの文字が複数のトークンに分割される可能性があります。そのため、個々のトークン
+            # をデコードすると不完全な文字になることがあります。
+            try:
+                current_text = self.encoder.decode(self.token_buffer, errors="strict")
 
-            # 新しく追加されたテキストのみを取得
-            new_text = current_text[len(self.last_text) :]
+                # 新しく追加されたテキストのみを取得
+                new_text = current_text[len(self.last_text) :]
 
-            # 新しいテキストがある場合のみコールバックを呼び出す
-            if new_text and self.callback:
-                self.callback(new_text)
+                # 新しいテキストがある場合のみコールバックを呼び出す
+                if new_text and self.callback:
+                    self.callback(new_text)
 
-            # 最後のテキストを更新
-            self.last_text = current_text
+                # 最後のテキストを更新
+                self.last_text = current_text
+            except UnicodeDecodeError:
+                pass
 
         return True  # 生成を続行
